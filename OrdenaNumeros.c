f7ffbee2ec83489d49de404488626ffb4c6d32f7
Alexandre Lopes <62675416+Apolref@users.noreply.github.com>
2022-06-10 20:32:51 -0300

    Update OrdenaNumeros.c

--- OrdenaNumeros.c
+++ OrdenaNumeros.c
@@ -3,43 +3,40 @@
 #include "matriz.h"
 
 
-int **OrdenaDigitos(int n, int **A,int posicao){
+void OrdenaDigitos(int n, int **A, int posicao){
 
   int B[10]={0,0,0,0,0,0,0,0,0,0}, digito=0;
+  int **C;
+
+  C = Alocar_matriz(n,2);
   
-  for(int i=0; i < n-1;){
+  for(int i=0; i < n ;i++){
     digito = A[i][0]/posicao;
     digito = digito%10;
     B[digito] = B[digito]+1;
-    i++;
   }
 
-  for (int i=1;i<9;){
+  for (int i=1;i<=9;i++){
     B[i]= B[i]+ B[i-1];
-    i++;
   }
 
-  int C[n][2];
-
-  for (int i=n-1; i==0;){
+  for (int i=n-1; i>=0;i--){
     digito = A[i][0]/posicao;
     digito = digito%10;
     B[digito] = B[digito]-1;
     C[B[digito]][0] = A[i][0];
     C[B[digito]][1] = A[i][1];
-    i--;
   }
 
   for (int i=0;i<n-1;i++){
     A[i][0] = C[i][0];
     A[i][1] = C[i][1];
-    i++;
   }
-
-  return A;
+  
+  Liberar_matriz(n,2,C);
 }
 
-int **OrdenaNumeros(int n,int **A) {
+void OrdenaNumeros(int n,int **A) {
   
   int maior = 0;
   for (int i=0;i<n-1;i++){
@@ -52,26 +49,4 @@ int **OrdenaNumeros(int n,int **A) {
     OrdenaDigitos(n,A,posicao);
     posicao=posicao*10;
   }
-  return A;
 }
-
-
-/*
-int main(void){
-  int **A;
-  int n = 4;
-
-  A = Alocar_matriz(n,2);
-  
-  for(int i=0;i<n-1;i++){
-    for (int j=0; j<1;j++){
-      printf ("%d",A[i][j]);
-    }
-    printf ("\n");
-  }
-
-  OrdenaNumeros(n,A);
-
-  A = Liberar_matriz(n,2,A);
-}
-*/
